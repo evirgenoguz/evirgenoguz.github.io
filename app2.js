bad2f8c073a870e8e77319a4576a4e036e836cbd
Oguz Evirgen <evirgenoguz@gmail.com>
2022-04-06 15:04:35 +0300

    need some changes

---
+++ app2.js
@@ -0,0 +1,7 @@
+//Navbar JS
+function toggleMenu(){
+    var menutoggle = document.querySelector ('.toggle');
+    var menu = document.querySelector ('.menu');
+    menuToggle.classList.toggle('active')
+    menuToggle.classList.toggle('active')
+}
