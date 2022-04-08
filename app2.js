9fb21baadd80dbb6465169083615f55c8289b8ac
Oguz Evirgen <evirgenoguz@gmail.com>
2022-04-08 23:13:28 +0300

    Sınav Öncesi Son Commit

--- app2.js
+++
@@ -1,7 +0,0 @@
-//Navbar JS
-function toggleMenu(){
-    var menutoggle = document.querySelector ('.toggle');
-    var menu = document.querySelector ('.menu');
-    menuToggle.classList.toggle('active')
-    menuToggle.classList.toggle('active')
-}
---
+++ main.js
@@ -0,0 +1,10 @@
+//Navbar JS
+function toggleMenu() {
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    
+    
+    menu.classList.toggle("active");
+    console.log("hocam çalışmıyor anlayamadım");
+}
